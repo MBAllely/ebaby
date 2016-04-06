5c585603414dbc87ea39027a13a8263c3b688bb2
Joseph Karasek and Marika Allely <joseph.karasek@gmail.com>
2016-04-06 10:31:15 -0700

    add form for updating admin side

--- app/components/baby-animal-admin.js
+++ app/components/baby-animal-admin.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateShowing: false,
   actions: {
     deleteBabyAnimal(baby) {
       this.sendAction('deleteBabyAnimal', baby);
+    },
+    toggleUpdateShowing() {
+      this.toggleProperty('updateShowing');
     }
   }
 });
