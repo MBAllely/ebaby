ccb98034a8007142aaf5dfc41e51aeec0d18ac7c
Joseph Karasek and Marika Allely <mballely@gmail.com>
2016-04-06 09:48:52 -0700

    add baby-animal-list and baby-animal components; display functionality on admin page

--- app/models/baby-animal.js
+++ app/models/baby-animal.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  price: DS.attr(),
+  price: DS.attr('number'),
   description: DS.attr('string'),
   image: DS.attr('string'), //URL for image
   category: DS.attr('string')
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('baby-animal');
+  },
   actions: {
     addBabyAnimal(params) {
       var newBaby = this.store.createRecord('baby-animal', params);
