83faa948bc21d2f958e81d1b3d8b1ddac61ec17c
Joseph Karasek and Marika Allely <mballely@gmail.com>
2016-04-06 09:06:02 -0700

    save to DB from admin create form

--- app/components/ebaby-admin-create.js
+++ app/components/ebaby-admin-create.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addBabyAnimal() {
-      console.log("Your action is hooked up");
+      var params = {
+        name: this.get('name') ? this.get('name') : "Elrond",
+        price: this.get('price') ? this.get('price') : 5,
+        description: this.get('description') ? this.get('description') : "A standard baby animal. Guaranteed fresh. Rainboots included.",
+        image: this.get('image') ? this.get('image') : "https://s-media-cache-ak0.pinimg.com/736x/27/be/30/27be3032541b2567ac6fd686ee8618ce.jpg",
+        category: this.get('category') ? this.get('category') : "Piglet"
+      };
+      this.sendAction('addBabyAnimal', params);
     }
   }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addBabyAnimal(params) {
+      var newBaby = this.store.createRecord('baby-animal', params);
+      newBaby.save();
+      this.transitionTo('admin');
+    }
+  }
 });
